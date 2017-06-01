51f51bb757bb5ab87416ca4f71febf145de30fcc
bchow1 <chowdhury.biswanath@gmail.com>
2017-06-01 09:37:41 -0400

    'Tried to extract text file using extractText but did not work.'

--- Data/readPdf.py
+++ Data/readPdf.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue May 30 11:30:49 2017
-
-@author: sid
+@author: bchow1
 """
+
 import os
-from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
+from PyPDF2 import PdfFileReader, PdfFileWriter
 
 
 # Main Program
@@ -14,19 +14,31 @@ if __name__ == '__main__':
   print os.getcwd()
   pdfFile = 'picknett_1981_dense_gas_dispersion_slopes.pdf'
   
-  dirName = pdfFile.replace('.pdf','')
-  if not os.path.exists(dirName):
-      os.makedirs(dirName)
+  outDir = pdfFile.replace('.pdf','')
+  if not os.path.exists(outDir):
+      os.makedirs(outDir)
   
   in_file_pdf = PdfFileReader(file(pdfFile, "rb"))
   print in_file_pdf.numPages
   
+  # Loop through the pages in the pdf file
   for i in xrange(in_file_pdf.numPages):
-      output = PdfFileWriter()
-      output.addPage(in_file_pdf.getPage(i))
-      out_file_pdf = "P" + str(i) + ".pdf" 
-      out_file = os.path.join(dirName,out_file_pdf)
-      print("Saving " + out_file)
-      outputStream = file(out_file, "wb")
-      output.write(outputStream)
+
+      
+      pageObj = in_file_pdf.getPage(i)
+      
+      # Write each page to a separate pdf file
+      pdfOut = PdfFileWriter()
+      pdfOut.addPage(pageObj)
+      out_file_pdf = os.path.join(outDir,"P"+str(i)+".pdf")
+      #print("Page %d"%i)
+      outputStream = file(out_file_pdf, "wb")
+      pdfOut.write(outputStream)
       outputStream.close()
+      
+      # txtOut
+      txtOut = open(os.path.join(outDir,"P"+str(i)+".txt"),"w")
+      pageTxt = pageObj.extractText()
+      txtOut.write(pageTxt)
+      txtOut.close()
+     
